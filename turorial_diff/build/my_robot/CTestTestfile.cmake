6f4c4250f432cd3af8fbce768f2ecc938a021c98
Ibuki Ono <8326dmc@gmail.com>
2025-10-23 10:51:30 +0900

    add imu data to simulation

--- turorial_diff/build/my_robot/CTestTestfile.cmake
+++ turorial_diff/build/my_robot/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/ibuki/turorial_diff/my_robot
-# Build directory: /home/ibuki/turorial_diff/build/my_robot
+# Source directory: /home/ibuki/ros2_diff_drive/ros2_diff_drive_sim/turorial_diff/my_robot
+# Build directory: /home/ibuki/ros2_diff_drive/ros2_diff_drive_sim/turorial_diff/build/my_robot
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
--- turorial_diff/build/my_robot/ament_cmake_environment_hooks/local_setup.sh
+++ turorial_diff/build/my_robot/ament_cmake_environment_hooks/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/ibuki/turorial_diff/install/my_robot"}
+: ${AMENT_CURRENT_PREFIX:="/home/ibuki/ros2_diff_drive/ros2_diff_drive_sim/turorial_diff/install/my_robot"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
